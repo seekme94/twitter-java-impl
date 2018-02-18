f8b31210bb2e11de1defb0b951d3f564c9299ee0
khubaib_ahmed <khubaib_ahmed@yahoo.com>
2018-02-18 14:16:17 +0500

    Added Secret file

---
+++ src/Secret.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+/**
+ * @author Khubaib
+ *
+ */
+public class Secret {
+	public static final String CONSUMER_KEY = "my_consumer_key";
+	public static final String CONSUMER_SECRET = "my_consumer_secret";
+	public static final String CONSUMER_TOKEN = "my_consumer_token";
+	public static final String CONSUMER_TOKEN_SECRET = "my_consumer_token_secret";
+}
